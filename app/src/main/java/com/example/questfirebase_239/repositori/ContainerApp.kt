5cce450d75079a5a1d8f4c5460508356e74a43be
fadd00 <andhikahutama9@gmail.com>
2026-01-02 22:48:16 +0700

    feat: add DefaultContainerApp implementation providing FirebaseRepositorySiswa

--- app/src/main/java/com/example/questfirebase_239/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questfirebase_239/repositori/ContainerApp.kt
@@ -3,3 +3,7 @@ package com.example.questfirebase_239.repositori
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy { FirebaseRepositorySiswa() }
+}
