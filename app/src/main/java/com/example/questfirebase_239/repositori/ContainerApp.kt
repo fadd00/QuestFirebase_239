afb954f7829e4d6b3b55dcf61cbf180965fb420b
fadd00 <andhikahutama9@gmail.com>
2026-01-02 22:50:03 +0700

    feat: add `AplikasiDataSiswa` class to initialize the dependency container.

--- app/src/main/java/com/example/questfirebase_239/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questfirebase_239/repositori/ContainerApp.kt
@@ -1,9 +1,19 @@
 package com.example.questfirebase_239.repositori
 
+import android.app.Application
+
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
 
 class DefaultContainerApp : ContainerApp {
     override val repositorySiswa: RepositorySiswa by lazy { FirebaseRepositorySiswa() }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
 }
