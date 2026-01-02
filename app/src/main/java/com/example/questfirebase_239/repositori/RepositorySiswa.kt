1752287037073b4f0000d1a064508aec7378af70
fadd00 <andhikahutama9@gmail.com>
2026-01-02 22:40:10 +0700

    feat: add Firebase implementation for `RepositorySiswa` with `FirebaseFirestore` initialization.

--- app/src/main/java/com/example/questfirebase_239/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_239/repositori/RepositorySiswa.kt
@@ -6,3 +6,8 @@ interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
 }
+
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+}
